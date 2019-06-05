04846c70a9949dea707ec785dd6bcf856e75a206
hasanneo <hasan.awad88@gmail.com>
2019-06-05 22:38:54 +0300

    Restored

--- GCM-Project/src/controller/LoginController.java
+++ GCM-Project/src/controller/LoginController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import java.awt.Frame;
-import java.awt.Label;
 import java.util.ArrayList;
 
 import javafx.application.Platform;
@@ -16,7 +15,6 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 public class LoginController {
-	ArrayList<String> serverObjects;
 	@FXML
 	private Button logInBtn;
 
@@ -31,6 +29,7 @@ public class LoginController {
 
 	@FXML
 	private PasswordField passwordTxt;
+
 	@FXML
 	void CancelClick(MouseEvent event) {
 		// get a handle to the stage
@@ -52,34 +51,16 @@ public class LoginController {
 	@FXML
 	void LogInClick(MouseEvent event) {
 		// should check more cases
-		if (passwordTxt.getText().isEmpty()||accountNameTxt.getText().isEmpty()) {
-			Alert alert = new Alert(AlertType.ERROR, "", ButtonType.OK);
-			alert.setContentText("Fill username and password.");
+		if (passwordTxt.getText().isEmpty()) {
+			Alert alert = new Alert(AlertType.ERROR, "Incorrect Password", ButtonType.OK);
+			alert.setContentText("Incorrect Password");
 			alert.showAndWait();
-			accountNameTxt.setStyle("-fx-border-color: red ; -fx-border-width: 1px ;");
 			passwordTxt.setStyle("-fx-border-color: red ; -fx-border-width: 1px ;");
 			passwordTxt.clear();
 		} else {
-			//get username and pass from the DB
 			DataBaseController.SelectFromTable("accounts", "username", accountNameTxt.getText());
-<<<<<<< HEAD
-			serverObjects=DataBaseController.clientCon.getList();
-			if(ValidateLogin(serverObjects.get(0).toString(),serverObjects.get(1).toString())) {
-
-			}else {
-
-			}
-=======
 			//saves the returned object in DataBaseController.clientCon.obj
 			//MAJD COMMENT
->>>>>>> branch 'master' of https://github.com/hasanneo/GCM-Project.git
-		}
-	}
-	boolean ValidateLogin(String username,String pass) {
-		if(username.equals(accountNameTxt.getText())&&pass.equals(passwordTxt.getText())) {
-			return true;
-		}else {
-			return false;
 		}
 	}
 
---
+++ GCM-Project/src/controller/MainController.java
@@ -0,0 +1,64 @@
+package controller;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import main.MainProgram;
+
+public class MainController {
+	@FXML // fx:id="search_text"
+    private TextField search_text; // Value injected by FXMLLoader
+
+    @FXML // fx:id="login_btn"
+    private Button login_btn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="register_btn"
+    private Button register_btn; // Value injected by FXMLLoader
+    
+    @FXML
+    void AccountClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void CatalogClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void MenuClick(MouseEvent event) {
+
+    }
+    @FXML
+    void LoginClick(ActionEvent event) {
+    //opening the login window
+    	 try {
+    	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/LogInScreen.fxml"));
+    	        Parent root1 = (Parent) fxmlLoader.load();
+    	        Stage stage = new Stage();
+    	        stage.setScene(new Scene(root1));  
+    	        stage.show();
+    	    } catch(Exception e) {
+    	        e.printStackTrace();
+    	    }
+
+    }
+
+    @FXML
+    void RegisterClick(ActionEvent event) {
+    	Alert alert = new Alert(AlertType.CONFIRMATION, "Register", ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+    	alert.showAndWait();
+    }
+}
