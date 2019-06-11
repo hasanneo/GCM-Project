9e1a0e6b600bd9687ba837b44561ac9374f8a0ae
jawadrammal <49551384+jawadrammal@users.noreply.github.com>
2019-06-11 16:14:58 +0300

    added tour Class, changed things in city Class and sql in dbcontroller

--- GCM-Project/src/entity/City.java
+++ GCM-Project/src/entity/City.java
@@ -1,5 +1,73 @@
 package entity;
 
-public class City {
+import java.util.ArrayList;
 
+public class City {
+	private ArrayList<Map> maps;
+	private ArrayList<Tour> tours;
+	private String cityName;
+
+	private int numberOfMaps;
+	private int numberOfPOI;
+	private int numberOfTours;
+	private int numberOfVersions;
+
+
+	public City(String cityName)
+	{
+		this.cityName=cityName;
+	}
+	
+	public City(String cityName,ArrayList<Map> maps,ArrayList<Tour> tours)
+	{
+		this.cityName=cityName;
+		this.maps=maps;
+		this.tours=tours;
+		numberOfTours=tours.size();
+		numberOfMaps=maps.size();
+	}
+	
+	
+	public ArrayList<Map> getMaps() {
+		return maps;
+	}
+	public void setMaps(ArrayList<Map> maps) {
+		this.maps = maps;
+	}
+	public ArrayList<Tour> getTours() {
+		return tours;
+	}
+	public void setTours(ArrayList<Tour> tours) {
+		this.tours = tours;
+	}
+	public String getCityName() {
+		return cityName;
+	}
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+	public int getNumberOfMaps() {
+		return numberOfMaps;
+	}
+	public void setNumberOfMaps(int numberOfMaps) {
+		this.numberOfMaps = numberOfMaps;
+	}
+	public int getNumberOfPOI() {
+		return numberOfPOI;
+	}
+	public void setNumberOfPOI(int numberOfPOI) {
+		this.numberOfPOI = numberOfPOI;
+	}
+	public int getNumberOfTours() {
+		return numberOfTours;
+	}
+	public void setNumberOfTours(int numberOfTours) {
+		this.numberOfTours = numberOfTours;
+	}
+	public int getNumberOfVersions() {
+		return numberOfVersions;
+	}
+	public void setNumberOfVersions(int numberOfVersions) {
+		this.numberOfVersions = numberOfVersions;
+	}
 }
