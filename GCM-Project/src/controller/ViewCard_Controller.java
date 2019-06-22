ee5d0d327d1382d2628dcf42e6443b7e6a6d32f2
Ebrah <Ebrah@Spectre>
2019-06-22 18:24:44 +0300

    Purchase history works for all users

--- GCM-Project/src/controller/ViewCard_Controller.java
+++ GCM-Project/src/controller/ViewCard_Controller.java
@@ -3,8 +3,6 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import entity.Account;
 import entity.PurchaseHistory;
